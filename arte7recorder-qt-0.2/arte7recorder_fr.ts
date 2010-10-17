f4ab2c406e71fdd4f277f3f0f11ee77425fb584d
Vincent Vande Vyvre <vins@swing.be>
2010-10-17 16:39:22 +0200

    Create version 0.2 branch Fix a Qt bug in Maverick.

---
+++ arte7recorder-qt-0.2/arte7recorder_fr.ts
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="setting.py" line="182"/>
+        <source>arte7recorder</source>
+        <translation>arte7recorder</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="185"/>
+        <source>Settings</source>
+        <translation>Préférences</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="188"/>
+        <source>Videos folder :</source>
+        <translation>Dossier des vidéos :</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="191"/>
+        <source>Browser</source>
+        <translation>Parcourir</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="194"/>
+        <source>To record the summaries automatically.</source>
+        <translation>Enregistrer automatiquement les résumés.</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="200"/>
+        <source>Size of preview&apos;s thumbnails :</source>
+        <translation>Taille des vignettes de la visionneuse :</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="203"/>
+        <source>Size of basket&apos;s thumbnails :</source>
+        <translation>Taille des vignettes du panier :</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="arte7_ui.py" line="132"/>
+        <source>Download</source>
+        <translation>Télécharger</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="710"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="157"/>
+        <source>Save text</source>
+        <translation>Sauver le résumé</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="200"/>
+        <source>
+
+    Connection to  http://arte7.arte.tv ...</source>
+        <translation>
+
+... Connexion à http://arte7.arte.tv ...</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="263"/>
+        <source>Downloading is not complete.</source>
+        <translation>Un téléchargement est en cours.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="266"/>
+        <source>Do you want to stop the loading  ?</source>
+        <translation>Voulez-vous arrêter le transfert ?</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="273"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="361"/>
+        <source>    Reading contents ...</source>
+        <translation>... Lectire des contenus ...</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="418"/>
+        <source>Right click for show the summary.</source>
+        <translation>Cliquez droit pour afficher le résumé.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="645"/>
+        <source>complete.</source>
+        <translation>teminé.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="652"/>
+        <source>Canceled</source>
+        <translation>Abandonné.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="655"/>
+        <source>Downloading canceled.</source>
+        <translation>Téléchargements abandonnés.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="660"/>
+        <source>Stopped on error : %s</source>
+        <translation>Arrêté sur erreur : %s</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="703"/>
+        <source>A video of the same name already exists in the destination folder.</source>
+        <translation>Une vidéo du même nom existe déjà dans ce dossier.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="707"/>
+        <source>Do you want to replace the existing video?</source>
+        <translation>Voulez-vous remplacer cette vidéo ?</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="713"/>
+        <source>Replace</source>
+        <translation>Remplacer</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="865"/>
+        <source>Retry later.</source>
+        <translation>Essayez plus tard.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="888"/>
+        <source>Arte7recorder</source>
+        <translation>Arte7recorder</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="915"/>
+        <source>&amp;Options</source>
+        <translation>&amp;Options</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="892"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichiers</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="894"/>
+        <source>&amp;Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="896"/>
+        <source>Add the selected video.</source>
+        <translation>Ajouter la vidéo au panier.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="899"/>
+        <source>Remove selected video(s) from the list</source>
+        <translation>Enlever la vidéo du panier.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="902"/>
+        <source>Move up.</source>
+        <translation>Monter.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="904"/>
+        <source>Move down.</source>
+        <translation>Descendre.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="906"/>
+        <source>Download now.</source>
+        <translation>Télécharger maintenant.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="909"/>
+        <source>Abort download.</source>
+        <translation>Arrêter les téléchargements</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="912"/>
+        <source>Save the pitch.</source>
+        <translation>Sauver le résumé.</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="917"/>
+        <source>&amp;Connection</source>
+        <translation>&amp;Connexion</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="919"/>
+        <source>&amp;About</source>
+        <translation>&amp;A propos</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="921"/>
+        <source>&amp;Download</source>
+        <translation>Télécharger</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="923"/>
+        <source>&amp;Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="arte7_ui.py" line="925"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+</context>
+</TS>
